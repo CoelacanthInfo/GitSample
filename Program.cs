06a08d24a0ae7085c41869b6f3330ad9b63955bd
Makoto Nishimura <mac.nishimura@gmail.com>
2021-06-29 07:11:01 +0900

    最初のコミット

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ namespace GitSample
 {
     class Program
     {
+        /// <summary>
+        /// コメントを追加
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
