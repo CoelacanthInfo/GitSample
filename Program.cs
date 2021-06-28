c9f936d08d585f4eb6316d365d117a1b518158ba
Makoto Nishimura <mac.nishimura@gmail.com>
2021-06-29 08:56:41 +0900

    Add B

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace GitSample
     class Program
     {
         /// <summary>
-        /// コメントを追加 A
+        /// コメントを追加 A B
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
